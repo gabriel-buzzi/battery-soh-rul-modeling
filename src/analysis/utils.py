e896c8e7db6e16ba41c7548d0b83040a44cf080b
Gabriel Buzzi Sanches <gabrielbuzzi2@gmail.com>
2025-07-27 15:59:11 -0300

    Data analysis notebooks

--- src/analysis/utils.py
+++ src/analysis/utils.py
@@ -1,4 +1,4 @@
-"""Util function for data analysis."""
+"""Util functions for data analysis."""
 
 from matplotlib import pyplot as plt
 
@@ -25,8 +25,10 @@ def get_elsevier_single_column_fig(height_mm=85):
     """
     width_mm = 85  # Single-column width
     fig, ax = plt.subplots(
-        figsize=(mm_to_inches(width_mm), mm_to_inches(height_mm))
+        figsize=(mm_to_inches(width_mm), mm_to_inches(height_mm)),
+        constrained_layout=True,
     )
+    ax.tick_params(axis="both", pad=4)
     return fig, ax
 
 
@@ -47,6 +49,53 @@ def get_elsevier_double_column_fig(height_mm=85):
     """
     width_mm = 170  # Double-column width
     fig, ax = plt.subplots(
-        figsize=(mm_to_inches(width_mm), mm_to_inches(height_mm))
+        figsize=(mm_to_inches(width_mm), mm_to_inches(height_mm)),
+        constrained_layout=True,
     )
+    ax.tick_params(axis="both", pad=4)
     return fig, ax
+
+
+def get_elsevier_figure_with_subplots(
+    n_rows,
+    n_cols,
+    column="single",
+    subplot_aspect_ratio=3 / 4,
+    spacing_factor=1.1,
+):
+    """Return a figure and axes grid for Elsevier-style multi-subplot figures.
+
+    Parameters
+    ----------
+        n_rows (int): Number of subplot rows
+        n_cols (int): Number of subplot columns
+        column (str): 'single' (85mm) or 'double' (170mm)
+        subplot_aspect_ratio (float): Height/width for each subplot
+        spacing_factor (float): Multiplier to add space between subplots
+
+    Returns
+    -------
+        fig, axes
+    """
+    if column == "single":
+        total_width_mm = 85
+    elif column == "double":
+        total_width_mm = 170
+    else:
+        raise ValueError("column must be 'single' or 'double'")
+
+    total_width_in = mm_to_inches(total_width_mm)
+
+    # Compute height based on subplot layout and spacing
+    subplot_width = total_width_in / n_cols
+    subplot_height = subplot_width * subplot_aspect_ratio
+    total_height_in = subplot_height * n_rows * spacing_factor
+
+    fig, axes = plt.subplots(
+        n_rows,
+        n_cols,
+        figsize=(total_width_in, total_height_in),
+        constrained_layout=True,
+    )
+
+    return fig, axes
