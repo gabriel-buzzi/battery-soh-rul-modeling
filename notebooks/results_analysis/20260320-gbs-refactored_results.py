2bd27a902b5ab976cf652563c322651c42324d94
Gabriel Buzzi Sanches <gabrielbuzzi2@gmail.com>
2026-03-22 18:10:03 -0300

    Add new figures and analysis

--- notebooks/results_analysis/20260320-gbs-refactored_results.py
+++ notebooks/results_analysis/20260320-gbs-refactored_results.py
@@ -24,18 +24,18 @@ SOH_ARTIFACTS = (
     / "modeling"
     / "target-soh__feature_hash-d3c068bcf5e7a4c6ed2306b1952bf61d5ebe1e1691881fc154114f0df9bdf07c__split_seed-42__model_name-extratrees__weighting_strategy-none__rk-e7ebb21c64"
 )
-RUL_NO_TEMP_ARTIFACTS = (
-    BASE_PATH
-    / "results_refactored"
-    / "modeling"
-    / "target-rul__feature_hash-82854e4bcdd504169be5162d100888a4a50cce5db7d665692a29ab5d23a5b70d__split_seed-42__model_name-extratrees__weighting_strategy-none__rk-4074892c0e"
-)
-SOH_NO_TEMP_ARTIFACTS = (
-    BASE_PATH
-    / "results_refactored"
-    / "modeling"
-    / "target-soh__feature_hash-82854e4bcdd504169be5162d100888a4a50cce5db7d665692a29ab5d23a5b70d__split_seed-42__model_name-extratrees__weighting_strategy-none__rk-357a381862"
-)
+# RUL_NO_TEMP_ARTIFACTS = (
+#     BASE_PATH
+#     / "results_refactored"
+#     / "modeling"
+#     / "target-rul__feature_hash-82854e4bcdd504169be5162d100888a4a50cce5db7d665692a29ab5d23a5b70d__split_seed-42__model_name-extratrees__weighting_strategy-none__rk-4074892c0e"
+# )
+# SOH_NO_TEMP_ARTIFACTS = (
+#     BASE_PATH
+#     / "results_refactored"
+#     / "modeling"
+#     / "target-soh__feature_hash-82854e4bcdd504169be5162d100888a4a50cce5db7d665692a29ab5d23a5b70d__split_seed-42__model_name-extratrees__weighting_strategy-none__rk-357a381862"
+# )
 
 # %% [markdown]
 # ## Optimization results
@@ -49,26 +49,14 @@ soh_optimize_history = pd.read_csv(soh_optimize_path / "cv_trials.csv")
 rul_optimize_history = pd.read_csv(rul_optimize_path / "cv_trials.csv")
 
 # %%
-w = 1
-rul_optimize_history["new_objective"] = (
-    rul_optimize_history["rmse_val_mean"]
-    + w * rul_optimize_history["overfit_gap_mean"]
-)
-
-soh_optimize_history["new_objective"] = (
-    soh_optimize_history["rmse_val_mean"]
-    + w * soh_optimize_history["overfit_gap_mean"]
-)
-
-# %%
 fig, ax = create_figure(layout_type="one_and_a_half")
 soh_best_trial = soh_optimize_history.loc[
-    soh_optimize_history["new_objective"].idxmin()
+    soh_optimize_history["objective"].idxmin()
 ]
 sns.scatterplot(
     x=soh_optimize_history["rmse_val_mean"],
     y=soh_optimize_history["overfit_gap_mean"],
-    hue=soh_optimize_history["new_objective"],
+    hue=soh_optimize_history["objective"],
     ax=ax,
 )
 ax.scatter(
@@ -85,12 +73,12 @@ plt.legend(title="Objective")
 # %%
 fig, ax = create_figure(layout_type="one_and_a_half")
 rul_best_trial = rul_optimize_history.loc[
-    rul_optimize_history["new_objective"].idxmin()
+    rul_optimize_history["objective"].idxmin()
 ]
 sns.scatterplot(
     x=rul_optimize_history["rmse_val_mean"],
     y=rul_optimize_history["overfit_gap_mean"],
-    hue=rul_optimize_history["new_objective"],
+    hue=rul_optimize_history["objective"],
     ax=ax,
 )
 ax.scatter(
@@ -106,10 +94,10 @@ plt.legend(title="Objective")
 
 # %%
 soh_best_trial = soh_optimize_history.loc[
-    soh_optimize_history["new_objective"].idxmin()
+    soh_optimize_history["objective"].idxmin()
 ]
 rul_best_trial = rul_optimize_history.loc[
-    rul_optimize_history["new_objective"].idxmin()
+    rul_optimize_history["objective"].idxmin()
 ]
 
 # %%
@@ -125,3 +113,11 @@ print(f"Best RUL params:\n{rul_best_trial[param_names].to_dict()}")
 
 # %% [markdown]
 # ## Final Model
+
+# %%
+soh_final_model_path = SOH_ARTIFACTS / "predict"
+rul_final_model_path = RUL_ARTIFACTS / "predict"
+
+# %%
+soh_final_model_preds = pd.read_parquet(soh_final_model_path)
+rul_final_model_preds = pd.read_parquet(rul_final_model_path)
